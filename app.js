1641574e64696d0f0c62e35deebbf759e535503f
diegoperezm <diegoperezcorreo@gmail.com>
2017-10-24 14:03:24 -0300

    Add reactjs and connect them

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// https://daveceddia.com/create-react-app-express-production/ oct-2017
+// serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/', routes );
 // app.use('/users', users);
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,22 @@
+var path = require('path');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET */
+router.get('/api', function(req, res, next) {
+  res.json( { title: 'Testing Express plus React' });
 });
+
+
+
+
+/** https://daveceddia.com/create-react-app-express-production/ oct-2017
+* The "catchall" handler: for any request that doesn't
+* match one above, send back React's index.html file.
+*/
+router.get('*', function(req, res)  {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
+
 
 module.exports = router;
